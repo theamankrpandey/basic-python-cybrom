5cafb506a6c92257399d7ab2c877d69b029790ab
Amanpandeya44 <ap5766708@gmail.com>
2025-11-07 12:23:36 +0530

    new code

---
+++ file_handling_i/o.py
@@ -0,0 +1,11 @@
+# f=open('n7.txt','x')
+# # print(f.name)
+# # print(f.mode)
+# print(f.encoding)
+# print(f.writable())
+# print(f.readable())
+# print(f.closed)
+
+
+# f=open('n7.txt','w')
+f=open('a1.txt','a')
