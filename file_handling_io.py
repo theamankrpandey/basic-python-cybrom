05fd97c8de8c21ee755a8370358b8e85b6304c03
Amanpandeya44 <ap5766708@gmail.com>
2025-11-08 17:14:48 +0530

    code update

--- file_handling_io.py
+++ file_handling_io.py
@@ -3,3 +3,67 @@ import keyword
 for i in range(len(keyword.kwlist)):
     if i == (len(keyword.kwlist)-1):
         print (keyword.kwlist[i])
+
+
+# f=open('n7.txt','x')
+# # print(f.name)
+# # print(f.mode)
+# print(f.encoding)
+# print(f.writable())
+# print(f.readable())
+# print(f.closed)
+
+
+# f=open('n7.txt','w')
+# f=open('a1.txt','a')
+# with open ('a1.txt','r')as f:
+#     data=f.read()
+#     print(data)
+
+
+# f=open('a1.txt','a')
+# data="this is python class"
+# f.write(data)
+# f.close()
+
+# f=open('a1.txt','a')
+# data='''this is
+#  python 
+#  class'''
+# f.write(data)
+# f.close()
+
+'''for multiple line write use wrietelines '''
+# f=open('a1.txt','a')
+# data=['python','java','php']
+# f.writelines(data)
+# f.close()
+
+# f=open('a1.txt','a')
+# data=['python\n','java\n','php\n']
+# f.writelines(data)
+# f.close()
+
+# f=open('a1.txt','a')
+# data=['python\n','java\n','php\n']
+# f.writelines(data)
+# f.close()
+
+# f=open("a5.txt",'a')
+# data='python\n'
+# data1='this is python\n'
+# data2='''this is
+# python
+# class\n'''
+# f.writelines([data,data1,data2])
+# f.close()
+
+
+f=open('a4.txt')
+# all_data=f.read()
+# print(all_data)
+
+
+data=f.readlines()
+print(data)
+f.close()
