61aec6dcaed75fc23ddcc0f689fc3d96e84c0bf1
Amanpandeya44 <ap5766708@gmail.com>
2025-11-09 18:50:21 +0530

    new cde

---
+++ DSA/Basic_programming.py
@@ -0,0 +1,18 @@
+# reverse a number
+# n=int(input("enter a number"))
+# def hello(n):
+#     count=0
+#     while n>0:
+#        count+=1
+#        n=n//10
+#     return count
+# hello(n)
+
+
+# count a digit
+# n=int(input("enter a number"))
+# count=0
+# while n>0:
+#     n=n//10
+#     count+=1
+# print(count)
