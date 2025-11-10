1879204d2deca5cee9e78e221d5eb1527cbfd0ec
Amanpandeya44 <ap5766708@gmail.com>
2025-11-10 21:51:09 +0530

    code update

--- DSA/Basic_programming.py
+++ DSA/Basic_programming.py
@@ -33,18 +33,34 @@
 
 
 # Armstrong Number
-n=int(input("enter a number"))
-count=0
-arm=0
-o=n
-while n>0:
-    n=n//10
-    count+=1
-while n>0:
-    digit=n%10
-    arm=arm+digit**count
-    n=n//10
-if o==arm:
-    print("arm")
-else:
-    print("not")
+# n=int(input("enter a number"))
+# count=0
+# arm=0
+# o=n
+# while n>0:
+#     n=n//10
+#     count+=1
+# while n>0:
+#     digit=n%10
+#     arm=arm+digit**count
+#     n=n//10
+# if o==arm:
+#     print("arm")
+# else:
+#     print("not")
+
+
+# print factor of numers
+n=int(input("enter a number to find the factor"))
+for i in range(1,(n//2)+1):
+    if n%i==0:
+        print(i)
+print(n)
+
+# store the factor of number in a list form
+# n=int(input("enter a number to find the factor"))
+# li=[]
+# for i in range(1,n+1):
+#     if n%i==0:
+#         li.append(i)
+# print(li)
