3c44716a2c1fde9660804bf4efbd051b0e2edb2a
Amanpandeya44 <ap5766708@gmail.com>
2025-11-11 17:16:34 +0530

    new code

---
+++ OOP/classandobject.py
@@ -0,0 +1,14 @@
+# class student:
+#     '''hhh'''
+# print(id(student))
+# obj1=student
+# print(id(obj1))
+# obj2=student
+# print(id(obj2))
+# print(student.__doc__)
+
+
+class student:
+    def show():
+        print("welcome")
+print(student.show())
