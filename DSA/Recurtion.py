bd9ba4c6c12a08300a0741e1fcdc78e330963abc
Amanpandeya44 <ap5766708@gmail.com>
2025-11-17 11:49:28 +0530

    code update

--- DSA/Recurtion.py
+++ DSA/Recurtion.py
@@ -1,21 +1,28 @@
 # head recursion
-count=0
-def hello():
-    global count
-    if count==4:
-        return
-    print("aman")
-    count+=1
-    hello()
-hello()
+# count=0
+# def hello():
+#     global count
+#     if count==4:
+#         return
+#     print("aman")
+#     count+=1
+#     hello()
+# hello()
 
 # tail recursion or backtracking
-count=0
-def hello():
-    global count
-    if count==4:
-        return
-    count+=1
-    hello()
-    print("aman")
-hello()
+# count=0
+# def hello():
+#     global count
+#     if count==4:
+#         return
+#     count+=1
+#     hello()
+#     print("aman")
+# hello()
+
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
+print(factorial(5))
