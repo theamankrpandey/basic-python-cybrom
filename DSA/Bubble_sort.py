8dfd248ec987aa5652704041bd868b980c3a5a95
Amanpandeya44 <ap5766708@gmail.com>
2025-11-24 19:09:54 +0530

    selection sort

--- DSA/Bubble_sort.py
+++ DSA/Bubble_sort.py
@@ -2,6 +2,6 @@ l=[2,4,1,3,5,6]
 n=len(l)
 for i in range(0,n):
     for j in range(i+1,n):
-        if l[i]<l[j]:
-            l[j],l[i]=l[i],l[j]
+        if l[j]<l[i]:
+            l[i],l[j]=l[j],l[i]
 print(l)
